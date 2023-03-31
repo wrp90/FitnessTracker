858e930d0cb70ab9a52e611876a6f97cc8e6e9c2
Will Papp <wpapp90@gmail.com>
2023-03-30 20:12:32 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const button = document.querySelector('.button');
+
+const APIcall = () => {
+  fetch("https://wger.de/api/v2/", {
+    headers: {
+      Authorization: "Token 4f79ebb6fbc51e404161b06d7aa339d854859d36"
+    }
+  }).then((response) => response.json()).then((data) => console.log(data));
+}
+
+button.addEventListener('click', () => {
+  APIcall();
+});
+
+
+
