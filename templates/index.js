0ced9051d836b511e8b53da14130b23ff5d56e74
Will Papp <wpapp90@gmail.com>
2023-04-04 20:22:31 -0500

    set up search bar to search api for workouts, will add autofill

--- templates/index.js
+++ templates/index.js
@@ -1,13 +1,28 @@
-
-const workoutbutton = document.querySelector('.workout-button')
+/// Global consts
+const workoutButton = document.querySelector('.workout-button');
+const searchInput = document.querySelector('.input');
 
 // API call function
-const APIcall = () => {
-  fetch("https://api.api-ninjas.com/v1/exercises", {
+const handleInput = (input) => {
+  console.log(input)
+  fetch(`https://api.api-ninjas.com/v1/exercises?muscle=${input}`, {
     headers: { 'X-Api-Key': '/a3wPcLBhSVR1BNq96vTjQ==oKDk6Uev3zRMZP6g'}
   }).then((response) => response.json()).then((data) => console.log(data));
-}
+};
 
-workoutbutton.addEventListener('click', () => {
-  APIcall();
+/// Event listener for the search button on the workout page
+workoutButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  handleInput(searchInput.value);
 });
+
+/// Input event listener that activates search when pressing enter key
+searchInput.addEventListener('keyup', (e) => {
+  if (e.keyCode == 13) {
+    e.preventDefault();
+    handleInput(searchInput.value);
+  }
+});
+
+
+
